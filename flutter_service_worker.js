5c63923e4cae00def195da7bb5e618f7a5ddbb2b
marcelors15 <marcelors15@gmail.com>
2021-12-25 14:35:06 -0300

    teste

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3d78defa989aad8fa3b58de06b31b1a4",
-"/": "3d78defa989aad8fa3b58de06b31b1a4",
-"main.dart.js": "b929d6733d8cfcc02f0baf6cf19a1208",
+"index.html": "b8ce8e63306be595b6127bc0452f2f53",
+"/": "b8ce8e63306be595b6127bc0452f2f53",
+"main.dart.js": "166825646a3a0046ad778f78fdf29f93",
 "manifest.json": "f3b5a08c6073aebe8244782c174d6f6b",
 "version.json": "90a8dca4661a9f399987ba65791e982b"
 };
